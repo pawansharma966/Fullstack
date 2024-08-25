37b05986678df9861e4b7626ae53cf70414ec38b
pawan sharma <pawan.sharma24041999@gmail.com>
2024-08-25 17:13:01 +0530

    add initial files for backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+                                  // start terminal :-  npm run dev
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <> 
+    <h1>pawan</h1>
+    
+    </>
+  ) 
+}
+
+export default App 
